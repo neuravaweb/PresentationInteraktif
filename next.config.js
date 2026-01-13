4f42efb18b1faeeef6d618978137cdff1d2ce3b1
EgeKorkmz <egeekorkmaaz@gmail.com>
2026-01-13 04:45:16 +0100

    Disable GitHub Actions workflow, use gh-pages for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'interactive-phising-sim';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}` : '',
   images: {
     unoptimized: true,
   },
